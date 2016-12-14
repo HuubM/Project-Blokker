0556ad62477065e8ecaadda4876bc399fc644aa3
michael <michael@MICHAEL>
2016-12-14 19:04:47 +0100

    Bruid Pagina

---
+++ ProjectBlokker/src/ProjectBlokker/Controllers/Bruid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectBlokker.Controllers
+{
+    public class Bruid : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
