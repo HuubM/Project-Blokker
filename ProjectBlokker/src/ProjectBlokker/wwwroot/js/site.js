76a6ea900c77a00c438aa28390f6622827821c47
Yakup <yakup93.g@gmail.com>
2016-12-15 12:00:35 +0100

    Datepicker toegevoegd

--- ProjectBlokker/src/ProjectBlokker/wwwroot/js/site.js
+++ ProjectBlokker/src/ProjectBlokker/wwwroot/js/site.js
@@ -1,2 +1,26 @@
-﻿// Write your Javascript code.
+﻿
+ $(function () {
+    $.fn.datepicker.dates['nl'] = {
+		days: ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"],
+		daysShort: ["zo", "ma", "di", "wo", "do", "vr", "za"],
+		daysMin: ["ZO", "MA", "DI", "WO", "DO", "VR", "ZA"],
+		months: ["JANUARI", "FEBRUARI", "MAART", "APRIL", "MEI", "JUNI", "JULI", "AUGUSTUS", "SEPTEMBER", "OKTOBER", "NOVEMBER", "DECEMBER"],
+		monthsShort: ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+		today: "Vandaag",
+		monthsTitle: "Maanden",
+		clear: "Wissen",
+		weekStart: 1,
+		format: "dd-mm-yyyy"
+	};
 
+    $('#datetimepicker').datepicker({
+     format: 'dd/mm/yyyy',
+     language: 'nl'
+    });
+
+    $('#datetimepicker').on("changeDate", function() {
+        $('#my_hidden_input').val(
+            $('#datepicker').datepicker('getFormattedDate')
+        );
+    });
+});
